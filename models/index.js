3c272b3905494616f2da106f4f434b0df4a2d884
Hiro Protagonist <shdesai118@gmail.com>
2021-11-20 14:33:57 -0700

    added product belongs to

--- models/index.js
+++ models/index.js
@@ -5,8 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 
 // Categories have many Products
+Product.belongsTo(Category, { 
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
 // Products belongToMany Tags (through ProductTag)
 
