9abdf8e0d6d8b639378676a9a8dde7849713e4df
Hiro Protagonist <shdesai118@gmail.com>
2021-11-20 14:35:19 -0700

    added rest of model/index.js code

--- models/index.js
+++ models/index.js
@@ -19,9 +19,17 @@ Product.belongsTo(Category, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-
+Category.hasMany(Product, { 
+  foreignKey: "category_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Product.belongsToMany(Tag, {through: ProductTag }, {
+  foreignKey: "ProductTag_id"
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag}, {
+  foreignKey: "TagProduct_id"
+});
 module.exports = {
   Product,
   Category,
